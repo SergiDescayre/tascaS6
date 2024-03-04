aa93320788e3b8c443b0a83a556fb607e0f0ff92
Sergi Descayre <lepetitdesca@gmail.com>
2024-03-04 13:08:32 +0100

    exercici 3 amb delay

--- tasca_S6/src/App.jsx
+++ tasca_S6/src/App.jsx
@@ -1,13 +1,19 @@
 
 import './App.css'
+
 import Index from './pages/Index'
+import Home from "./pages/Home"
 import ContextProvider from './context/PresContext'
 
 
 function App() {
   return (
     <ContextProvider>
+   
+
+
       <Index />
+      <Home />
     </ContextProvider>
   )
 }
--- tasca_S6/src/context/PresContext.jsx
+++ tasca_S6/src/context/PresContext.jsx
@@ -1,45 +1,56 @@
-import { createContext, useState, useContext } from "react";
+import React, { createContext, useState, useContext, useEffect } from "react";
 import { infoPresupost } from "../data/variables";
 
 const PresContext = createContext();
 
 const ContextProvider = ({ children }) => {
-  const [totalPresupost, setTotalPresupost] = useState(300);
+  const [totalPresupost, setTotalPresupost] = useState(0);
   const [presupost, setPresupost] = useState(infoPresupost);
+
+
+  const updateTotalPresupost = () => {
+    setTotalPresupost( presupost.reduce((a,b) =>a + b.totalPrice,0))
+  }
+
+  useEffect(()=> {
+    updateTotalPresupost()
+  },[presupost])
+
 
   const handleChecked = (id) => {
     setPresupost(
       presupost.map((press) =>
-        press.id === id ? { ...press, check: true } : press
+        press.id === id ? { ...press, check: !press.check, totalPrice: ((press.numberLanguage + press.numberPage)*30) + press.price } : press
       )
     );
-    
   };
 
   const addPage = (id) => {
     setPresupost(
-        presupost.map(press => 
-        press.id===id ? { ...press, numberPage:press.numberPage+1} : press)
+      presupost.map(press =>
+        press.id === id ? { ...press, numberPage: press.numberPage + 1, totalPrice: ((press.numberLanguage + press.numberPage)*30) + press.price } : press)
     )
+
   };
+
   const lessPage = (id) => {
     setPresupost(
-        presupost.map(press => 
-        (press.id===id && press.numberPage !==0) ? { ...press, numberPage:press.numberPage-1} : press)
+      presupost.map(press =>
+        (press.id === id && press.numberPage !== 0) ? { ...press, numberPage: press.numberPage - 1 , totalPrice: ((press.numberLanguage + press.numberPage)*30) + press.price } : press)
     )
   };
 
   const addLanguage = (id) => {
     setPresupost(
-        presupost.map(press => 
-        press.id===id ? { ...press, numberLanguage:press.numberLanguage+1} : press)
+      presupost.map(press =>
+        press.id === id ? { ...press, numberLanguage: press.numberLanguage + 1 , totalPrice: ((press.numberLanguage + press.numberPage)*30) + press.price } : press)
     )
   };
 
   const lessLanguage = (id) => {
     setPresupost(
-        presupost.map(press => 
-        (press.id===id && press.numberLanguage !==0) ? { ...press, numberLanguage:press.numberLanguage-1} : press)
+      presupost.map(press =>
+        (press.id === id && press.numberLanguage !== 0) ? { ...press, numberLanguage: press.numberLanguage - 1, totalPrice: ((press.numberLanguage + press.numberPage)*30) + press.price } : press)
     )
   };
 
